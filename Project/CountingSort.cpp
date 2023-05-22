df04be9923c67534ee0d4f66c2136f21178db56b
AhnYeonghoo <dksdudgn00@naver.com>
2023-05-22 11:29:34 +0900

    카운트소팅

---
+++ Project/CountingSort.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+void countSort(vector<int>& input)
+{
+	map<int, int> freq;
+
+	for (int x : input)
+	{
+		freq[x]++;
+	}
+	
+	int i = 0;
+
+	for (auto& p : freq)
+	{
+		while (p.second--)
+		{
+			input[i++] = p.first;
+		}
+	}
+}
+
+int main()
+{
+	vector<int> input = { 4, 2, 1, 4, 1, 4, 2, 1, 10 };
+	countSort(input);
+
+	for (auto i : input)
+		cout << i << " ";
+
+	return 0;
+}
--- Project/main.cpp
+++ Project/main.cpp
@@ -1,6 +1,6 @@
 #include "Stack.h"
 
-int main()
+int maintemp()
 {
 	Stack pt(3);
 	
